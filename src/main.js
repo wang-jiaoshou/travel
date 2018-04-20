e4e8aab9244cee31ac8bc7b58f4265acbf9d5a52
whk <478447095@qq.com>
2018-04-20 19:17:34 +0800

    project init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+
+// 1像素边框的问题
+
+Vue.config.productionTip = false
+// 移动端300ms点击延迟
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+// 路由就是根据网址的不同，返回不同的内容给用户
