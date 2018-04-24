721a9035969ddfb7053a8c8e0ae09dba57be2c83
whk <478447095@qq.com>
2018-04-25 00:44:22 +0800

    index finsh

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper>
+    <swiper :options="swiperOption">
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <div
         class="icon"
@@ -8,7 +8,7 @@
         :key="item.id"
         >
           <div class="icon-img">
-            <img  class="icon-img-content" src="item.imgUrl" alt="">
+            <img  class="icon-img-content" :src="item.imgUrl">
           </div>
           <p class="icon-desc">{{item.desc}}</p>
            </div>
@@ -22,59 +22,20 @@
 <script>
 export default {
   name: 'HomeIcons',
+  props: {
+    list: Array
+  },
   data () {
     return {
-      iconList: [{
-        id: '001',
-        imgUrl: '../components/q1.jpg',
-        desc: '热门景点'
-      }, {
-        id: '002',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '003',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '004',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '005',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '006',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '007',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '008',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '009',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '010',
-        imgUrl: '../components/q1.jpg',
-        desc: '滑雪季'
-      }, {
-        id: '011',
-        imgUrl: '../components/q1.jpg',
-        desc: '一日游一日游一日游'
-      }]
+      swiperOption: {
+        autoplay: false
+      }
     }
   },
   computed: {
     pages () {
       const pages = []
-      this.iconList.forEach((item, index) => {
+      this.list.forEach((item, index) => {
         const page = Math.floor(index / 8)
         if (!pages[page]) {
           pages[page] = []
@@ -94,35 +55,37 @@ export default {
       height: 0
       padding-bottom: 50%
     // background: green
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      // background: red
-      .icon-img
-        position:absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        // background: blue
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        ellipsis()
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
+    .icons
+      margin-top: .1rem
+      .icon
+        position: relative
+        overflow: hidden
+        float: left
+        width: 25%
+        height: 0
+        padding-bottom: 25%
+        // background: red
+        .icon-img
+          position:absolute
+          top: 0
+          left: 0
+          right: 0
+          bottom: .44rem
+          box-sizing: border-box
+          padding: .1rem
+          // background: blue
+          .icon-img-content
+            display: block
+            margin: 0 auto
+            height: 100%
+        .icon-desc
+          ellipsis()
+          position: absolute
+          left: 0
+          right: 0
+          bottom: 0
+          height: .44rem
+          line-height: .44rem
+          text-align: center
+          color: $darkTextColor
 </style>
